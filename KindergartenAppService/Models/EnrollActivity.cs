194400c4a5f431477453fb69b37e24276ca4a1f9
gfajardo <gfajardo@DESKTOPTIGIAME>
2019-08-07 19:07:07 -0400

    erollment activities views

--- KindergartenAppService/Models/EnrollActivity.cs
+++ KindergartenAppService/Models/EnrollActivity.cs
@@ -4,9 +4,13 @@ namespace KindergartenAppService.Models
 {
     public class EnrollActivity:UniqueEntity
     {
+        [Display(Prompt = "", Name = "Actividad")]
         public Guid ActivityId{ get; set; }
+        [Display(Prompt = "", Name = "Actividad")]
         public Activity Activity { get; set; }
+        [Display(Prompt = "", Name = "Insripción")]
         public virtual Guid EnrollmentId { get; set; }
+        [Display(Prompt = "", Name = "Insripción")]
         public virtual Enrollment Enrollment { get; set; }
     }
 }
