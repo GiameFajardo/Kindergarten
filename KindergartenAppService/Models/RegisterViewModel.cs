b90ad9384c19f9acd54b2dff533891b23fea5fe3
Giame Fajardo <gmcrls13@gmail.com>
2019-10-27 18:43:25 -0400

    RegistrationVireModel created

---
+++ KindergartenAppService/Models/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KindergartenAppService.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Correo")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmación")]
+        [Compare("Password",ErrorMessage = "Contraseña y confirmacion no son iguales.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
