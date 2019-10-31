798c23fbbbd5a6671e50990262399dd99a6f251d
Giame Fajardo <gmcrls13@gmail.com>
2019-10-31 06:48:29 -0400

    Creating role

---
+++ KindergartenAppService/Controllers/AdministrationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KindergartenAppService.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+    }
+}
